dc7a2da638559a3fa189aeea3c113a141f624369
EmmanuelBourdier <emmanuelbourdier@hotmail.fr>
2025-05-15 19:05:27 +0200

    section slide suite

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,11 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request, EntityManagerInterface $entityManagerInterface):Response
+    public function index(Request $request, EntityManagerInterface $entityManagerInterface,MailerInterface $mailer):Response
     {
         $user = new User();
                 $form = $this->createForm(RegisterUserType::class, $user);
@@ -21,11 +23,26 @@ class RegisterController extends AbstractController
                 if($form->isSubmitted() && $form->isValid()){
                     $user = $form->getData();
                     $user->setRoles(['ROLE_USER']);
+                    $plainPassword = $form->get('plainPassword')->getData();
                     //figer les données
                     $entityManagerInterface->persist($user);
                     //envoyer les données
                     $entityManagerInterface->flush();
-        
+                    $email = (new Email())
+                    ->from('noreply@demomailtrap.co')
+                    ->to($user->getEmail())
+                    ->subject('Bienvenue sur notre plateforme')
+                    ->html(
+            $this->renderView(
+                'emails/welcome.html.twig',
+                ['user' => $user, 'password' => $plainPassword]
+            )
+        );
+    
+    $mailer->send($email);
+                    // Add this redirect
+                    $this->addFlash('success', 'Votre compte a été créé avec succès. Vous pouvez maintenant vous connecter.');
+                    return $this->redirectToRoute('app_login');        
                 }
                 return $this->render('register/index.html.twig',['form' => $form->createView()
     ]);
